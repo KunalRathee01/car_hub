d368ef3ce3c5784e80144ba5847f7eeea3517260
KunalRathee01 <rathikunal018@gmail.com>
2024-02-29 02:34:30 +0530

    first commit

---
+++ next.config.mjs
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    images: {
+        domains: ["cdn.imagin.studio"]
+    }
+};
+
+// Export the configuration as default
+export default nextConfig;
